af778d6bccc9816e60ae61bc13fed2fb4a1cb684
Guilherme Miyamoto Bragatto <99208444+bragatto-tec@users.noreply.github.com>
2026-08-26 11:34:46 +0000

    mudanças para lab2

--- lab2/core/src/main/java/br/mackenzie/Main.java
+++ lab2/core/src/main/java/br/mackenzie/Main.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -17,16 +19,23 @@ public class Main implements ApplicationListener {
     FitViewport viewport;
     Sprite bucketSprite;
 
+    Texture backgroundTexture;
+    Vector2 touchPos;
+
 
 
     @Override
     public void create() {
         bucketTexture = new Texture("bucket.png");
+        backgroundTexture = new Texture("background.png");
+        
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
 
         bucketSprite = new Sprite(bucketTexture);
         bucketSprite.setSize(1, 1);
+
+        touchPos = new Vector2();
 
         // Prepare your application here.
     }
@@ -53,6 +62,13 @@ public class Main implements ApplicationListener {
         } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             bucketSprite.translateX(-speed * delta); 
         }
+        
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
+            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
+            bucketSprite.setCenterX(touchPos.x); // Change the horizontally centered position of the bucket
+        }
+        
     
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             bucketSprite.translateY(speed * delta); 
@@ -62,15 +78,21 @@ public class Main implements ApplicationListener {
     }
 
     private void logic() {
-
+        float worldWidth = viewport.getWorldWidth();
+        float bucketWidth = bucketSprite.getWidth();
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(),0,worldWidth));
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.RED);
+        ScreenUtils.clear(Color.BLACK);
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
         spriteBatch.begin();
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        spriteBatch.draw(backgroundTexture,0,0,worldWidth,worldHeight);
+
         bucketSprite.draw(spriteBatch);
         spriteBatch.end();
     }
