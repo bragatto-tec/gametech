e5ba36bcd699d8468fd390d221d65d1fb60da01c
Guilherme Miyamoto Bragatto <99208444+bragatto-tec@users.noreply.github.com>
2026-09-02 18:36:42 +0000

    lab04

--- lab2/core/src/main/java/br/mackenzie/Main.java
+++ lab2/core/src/main/java/br/mackenzie/Main.java
@@ -11,8 +11,8 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.Array;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
     Texture bucketTexture;
     SpriteBatch spriteBatch;
@@ -21,14 +21,17 @@ public class Main implements ApplicationListener {
 
     Texture backgroundTexture;
     Vector2 touchPos;
+    Texture dropTexture;
 
-
+    Array<Sprite> dropSprites;
+    float dropTimer;
 
     @Override
     public void create() {
         bucketTexture = new Texture("bucket.png");
         backgroundTexture = new Texture("background.png");
-        
+        dropTexture = new Texture("drop.png");
+
         spriteBatch = new SpriteBatch();
         viewport = new FitViewport(8, 5);
 
@@ -37,55 +40,78 @@ public class Main implements ApplicationListener {
 
         touchPos = new Vector2();
 
-        // Prepare your application here.
+        dropSprites = new Array<>();
+        createDroplet();
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true); // true centers the camera
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        // Draw your application here.
         input();
         logic();
         draw();
     }
 
     private void input() {
-    float speed = 4f;
-    float delta = Gdx.graphics.getDeltaTime();
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
-    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-        bucketSprite.translateX(speed * delta);
-    } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-        bucketSprite.translateX(-speed * delta);
-    }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
 
-    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-        bucketSprite.translateY(speed * delta);
-    } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-        bucketSprite.translateY(-speed * delta);
-    }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            bucketSprite.translateY(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            bucketSprite.translateY(-speed * delta);
+        }
 
-    if (Gdx.input.isTouched()) {
-        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-        viewport.unproject(touchPos);
-        bucketSprite.setCenterX(touchPos.x);
-        bucketSprite.setCenterY(touchPos.y);
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+            bucketSprite.setCenterY(touchPos.y);
+        }
     }
-}
 
     private void logic() {
-    float worldWidth = viewport.getWorldWidth();
-    float worldHeight = viewport.getWorldHeight();
-    float bucketWidth = bucketSprite.getWidth();
-    float bucketHeight = bucketSprite.getHeight();
+        if (bucketSprite == null) {
+            return;
+        }
 
-    bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
-    bucketSprite.setY(MathUtils.clamp(bucketSprite.getY(), 0, worldHeight - bucketHeight));
-}
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+        bucketSprite.setY(MathUtils.clamp(bucketSprite.getY(), 0, worldHeight - bucketHeight));
+
+        float delta = Gdx.graphics.getDeltaTime();
+
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.translateY(-2f * delta);
+
+            if (bucketSprite.getBoundingRectangle().overlaps(dropSprite.getBoundingRectangle())) {
+                bucketSprite = null;
+                dropSprites.removeValue(dropSprite, true);
+                break;
+            }
+        }
+
+        dropTimer += delta;
+
+        if (dropTimer > 1f) {
+            dropTimer = 0;
+            createDroplet();
+        }
+    }
 
     private void draw() {
         ScreenUtils.clear(Color.BLACK);
@@ -93,26 +119,51 @@ public class Main implements ApplicationListener {
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
         spriteBatch.begin();
+
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
-        spriteBatch.draw(backgroundTexture,0,0,worldWidth,worldHeight);
 
-        bucketSprite.draw(spriteBatch);
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+
+        if (bucketSprite != null) {
+            bucketSprite.draw(spriteBatch);
+        }
+
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
+    }
+
+    private void createDroplet() {
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidth, dropHeight);
+
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+
+        dropSprites.add(dropSprite);
     }
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
     }
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        bucketTexture.dispose();
+        backgroundTexture.dispose();
+        dropTexture.dispose();
+        spriteBatch.dispose();
     }
 }
